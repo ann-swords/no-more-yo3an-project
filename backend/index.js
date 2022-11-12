9c097ef9ca423f1ebda72defdc8b6c83b22fb3ff
Anwaar <anwaaralsyouf@gmail.com>
2022-11-12 19:19:22 +0300

    added cors

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,13 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 require('dotenv').config()
 require('./config/database')
 
+//Middleware:
+app.use(cors())
 app.use(express.json())
+
 app.use('/', require('./routes/users'))
 app.use('/', require('./routes/food'))
 
