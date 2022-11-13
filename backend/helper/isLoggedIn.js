e63fcb05b37026d71216f1dafffc63a25bf45e74
Anwaar <anwaaralsyouf@gmail.com>
2022-11-13 15:11:42 +0300

    fix in token authentication

---
+++ backend/helper/isLoggedIn.js
@@ -0,0 +1,29 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+module.exports = (req, res, next) => {
+    // const token = req.header("x-auth-token");
+    // console.log(token);
+
+    let token = ""
+    let authorizationToken = req.header("Authorization");
+    console.log(authorizationToken);
+
+    if(!authorizationToken){
+        return res.status(401).json({ message : "Ahaaan!!! You are not allowed to view this as this is a protected route."})
+    }
+
+    if(authorizationToken){
+        authorizationToken = authorizationToken.replace("Bearer ", "");
+        console.log(authorizationToken);
+        token = authorizationToken;
+    }
+
+    try{
+        const decoded = jwt.verify(token, process.env.SECRET);
+        req.user = decoded.user;
+        next(); //move to the next instruction
+    } catch(err){
+        return res.status(401).json({message: "Your token is invalid."})
+    }
+};
