01480d918139dd6e87cd7308a3a095aa054bb2c7
sumayajassim <sumayajassim1@gmail.com>
2022-11-13 13:33:13 +0300

    add homepage component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,52 @@
 import './App.css';
 import Signup from './components/Signup/Signup';
 import Login from './components/Login/Login';
-
+import HomePage from './components/HomePage/HomePage'; 
 import { useState, useEffect} from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import jwt_decode from "jwt-decode";
 import axios from "axios";
 
+
+
+
+function App() {
+  const [isAuth, setIsAuth] = useState(false);
+  const [user, setUser] = useState({});
+
+
+  useEffect(()=>{
+    let token = localStorage.getItem("token");
+    if(token != null){
+      let user = jwt_decode(token);
+
+      if(user){
+        setIsAuth(true);
+        setUser(user);
+      }else if(!user){
+        localStorage.removeItem("token");
+        setIsAuth(false);
+      }
+    }
+  },[])
 //User Authentication:
+
+
+const loginHandler = (cred) =>{
+  axios.post("http://localhost:4000/auth/signin")
+  .then(res =>{
+    if(res.data.token != null){
+      localStorage.setItem("token", res.data.token);
+      let user = jwt_decode(res.data.token);
+      setIsAuth(true);
+      setUser(user);
+    }
+  })
+  .catch(err => {
+    console.log(err);
+  })
+}
+
 
 //Add a new user:
 const registerHandler = (user) => {
@@ -20,16 +59,13 @@ const registerHandler = (user) => {
       console.log(err);
     });
 };
-
-
-function App() {
   return (
     <Router>
     <div className="App">
       <Routes>
         {/* <Route path="*" element={<HomePage />} /> */}
         <Route path="/signup" element={<Signup register={registerHandler}/>} />
-        <Route path="/login" element={ <Login />} />
+        <Route path="/login" element={isAuth? <HomePage></HomePage> : <Login login={loginHandler}/>} />
       </Routes>
     </div>
   </Router>
